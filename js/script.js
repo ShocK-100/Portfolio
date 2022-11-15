9c070f2dfef0b0be23a07cee2498d9362c5ed0dd
ShocK-100 <talzvi99@gmail.com>
2022-11-16 00:08:38 +0200

    All sections done

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ $(document).ready(() => {
     $("header").toggleClass("toggle");
   });
 });
+
+$(window).on("scroll load", function () {
+  $("#menu").removeClass("fa-times");
+  $("header").removeClass("toggle");
+});

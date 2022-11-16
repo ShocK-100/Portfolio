b9cfef790b35018abc270582c3657cb88d748513
ShocK-100 <talzvi99@gmail.com>
2022-11-16 18:40:18 +0200

    navigation items are now colored upon click

--- js/script.js
+++ js/script.js
@@ -3,23 +3,51 @@ $(document).ready(() => {
     $(this).toggleClass("fa-times");
     $("header").toggleClass("toggle");
   });
+
+  let navigationItem = $("header")
+    .find(".navbar")
+    .find("ul")
+    .find("li")
+    .find("a");
+
+  navigationItem.click(function (e) {
+    navigationItem.each(function () {
+      console.log($(this));
+      $(this).removeClass("toggle-nav-item");
+    });
+    let navID = e.target.id;
+    navigationItem
+      .parent()
+      .find("#" + `${navID}`)
+      .toggleClass("toggle-nav-item");
+  });
 });
 
 $(window).on("scroll load", function () {
   $("#menu").removeClass("fa-times");
   $("header").removeClass("toggle");
 
-  if ($(window).scrollTop() > 0) {
+  let windowScrollTop = $(window).scrollTop();
+  if (windowScrollTop > 0) {
     $(".top").show();
   } else {
     $(".top").hide();
   }
+
+  // if (windowScrollTop >= 0 && windowScrollTop < 691.2000122070312) {
+  //   console.log("true");
+  //   console.log($("header").find("nav").find("ul").find("nav-home"));
+  //   $("header")
+  //     .find("nav")
+  //     .find("ul")
+  //     .find("#nav-home")
+  //     .toggleClass("toggle-nav-item");
+  // }
 });
 
 // smooth scrolling
 
 $('a[href*="#"]').on("click", function (e) {
-  console.log(e);
   e.preventDefault();
   $("html, body").animate(
     {

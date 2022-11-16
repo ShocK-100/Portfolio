065b4f527ed05dce3c80af85f0bb7ea916d2c0a6
ShocK-100 <talzvi99@gmail.com>
2022-11-16 13:12:09 +0200

    website works except email form

--- js/script.js
+++ js/script.js
@@ -8,4 +8,24 @@ $(document).ready(() => {
 $(window).on("scroll load", function () {
   $("#menu").removeClass("fa-times");
   $("header").removeClass("toggle");
+
+  if ($(window).scrollTop() > 0) {
+    $(".top").show();
+  } else {
+    $(".top").hide();
+  }
+});
+
+// smooth scrolling
+
+$('a[href*="#"]').on("click", function (e) {
+  console.log(e);
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top,
+    },
+    500,
+    "linear"
+  );
 });

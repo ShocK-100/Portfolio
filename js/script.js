baefb6648811cbd50b87c2a4bf7087663c7f4b02
ShocK-100 <talzvi99@gmail.com>
2022-11-16 21:53:18 +0200

    scrolling section done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let numOfHeaderItems;
+
 $(document).ready(() => {
   $("#menu").click(function () {
     $(this).toggleClass("fa-times");
@@ -5,9 +7,12 @@ $(document).ready(() => {
   });
 
   $(".nav-item").click(function (e) {
-    $(".nav-item").removeClass("toggle-nav-item");
-    $("#" + `${e.target.id}`).toggleClass("toggle-nav-item");
+    $(".toggled-nav-item").removeClass("toggled-nav-item");
+    $("#" + `${e.target.id}`).addClass("toggled-nav-item");
   });
+
+  numOfHeaderItems = $(".nav-item").length;
+  numOfHeaderItems--;
 });
 
 $(window).on("scroll load", function () {
@@ -20,27 +25,57 @@ $(window).on("scroll load", function () {
   } else {
     $(".top").hide();
   }
+  console.log(windowScrollTop);
 
-  // if (windowScrollTop >= 0 && windowScrollTop < 691.2000122070312) {
-  //   console.log("true");
-  //   console.log($("header").find("nav").find("ul").find("nav-home"));
-  //   $("header")
-  //     .find("nav")
-  //     .find("ul")
-  //     .find("#nav-home")
-  //     .toggleClass("toggle-nav-item");
-  // }
+  let scrollablePixels =
+    document.documentElement.offsetHeight - window.innerHeight;
+  let aboutMinThreshold = scrollablePixels / numOfHeaderItems;
+  let educationMinThreshold = (scrollablePixels / numOfHeaderItems) * 2;
+  let portfolioMinThreshold = (scrollablePixels / numOfHeaderItems) * 3;
+  let contactMinThreshold = (scrollablePixels / numOfHeaderItems) * 4;
+
+  switch (true) {
+    case windowScrollTop >= 0 && windowScrollTop < aboutMinThreshold:
+      $(".toggled-nav-item").removeClass("toggled-nav-item");
+      $("#home-nav-id").addClass("toggled-nav-item");
+      break;
+
+    case windowScrollTop >= aboutMinThreshold &&
+      windowScrollTop < educationMinThreshold:
+      $(".toggled-nav-item").removeClass("toggled-nav-item");
+      $("#about-nav-id").addClass("toggled-nav-item");
+      break;
+
+    case windowScrollTop >= educationMinThreshold &&
+      windowScrollTop < portfolioMinThreshold:
+      $(".toggled-nav-item").removeClass("toggled-nav-item");
+      $("#education-nav-id").addClass("toggled-nav-item");
+      break;
+
+    case windowScrollTop >= portfolioMinThreshold &&
+      windowScrollTop < contactMinThreshold:
+      $(".toggled-nav-item").removeClass("toggled-nav-item");
+      $("#portfolio-nav-id").addClass("toggled-nav-item");
+      break;
+
+    case windowScrollTop >= contactMinThreshold:
+      $(".toggled-nav-item").removeClass("toggled-nav-item");
+      $("#contact-nav-id").addClass("toggled-nav-item");
+      break;
+  }
 });
 
-// smooth scrolling
+// *Smooth scrolling* - removed because it damages the scroll & nav-item click.
+// Meaning that due to the smooth scroll the page scrolls differently every nav-item click
+// hence the threshold for changinf the highlight is different with each click
 
-$('a[href*="#"]').on("click", function (e) {
-  e.preventDefault();
-  $("html, body").animate(
-    {
-      scrollTop: $($(this).attr("href")).offset().top,
-    },
-    500,
-    "linear"
-  );
-});
+// $('a[href*="#"]').on("click", function (e) {
+//   e.preventDefault();
+//   $("html, body").animate(
+//     {
+//       scrollTop: $($(this).attr("href")).offset().top,
+//     },
+//     500,
+//     "linear"
+//   );
+// });
